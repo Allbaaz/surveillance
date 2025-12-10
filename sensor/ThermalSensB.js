04cdb8169ca633e6dabdbe5dcb6b599ce9428770
Allbaaz <clsm.louis@gmail.com>
2025-12-10 15:26:21 +0100

    Ajout du thermal B avec le design pattern Adapter

---
+++ sensor/ThermalSensB.js
@@ -0,0 +1,14 @@
+ThermalSensorB
+class ThermalSensorB {
+ constructor(position) {
+    this.position = position;
+ } // position {
+ scanHeatSignature() {
+    return { sensor: "ThermalSensorB",
+        detection: "Thermal",
+        date: new Date().toISOString(),
+    };
+ } // Déclenche une donnée complexe json, voir ci dessous
+}
+
+export { ThermalSensorB };
