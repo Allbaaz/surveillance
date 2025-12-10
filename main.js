c4051250bfa1c75dccffd28eba7f415e1f847beb
Allbaaz <clsm.louis@gmail.com>
2025-12-10 16:19:34 +0100

    Ajout du main.js et fin de la partie 3 de l'exercice

---
+++ main.js
@@ -0,0 +1,33 @@
+import { Room } from "./room/Room.js";
+
+import { ConsoleObserver } from "./notifications/ConsoleObserv.js";
+import { EmailObserver } from "./notifications/EmailObserv.js";
+import { DiscordObserver } from "./notifications/DiscordObserv.js";
+import { LogObserv } from "./notifications/LogObserv.js";
+
+import { CamAdapter } from "./sensor/CamAdapter.js";
+import { TempSensAdapter } from "./sensor/TempSensAdapter.js";
+import { MotionSensAdapter } from "./sensor/MotionSensAdapter.js";
+import { ThermalSensBAdapter } from "./sensor/ThermalSensBAdapter.js";
+
+const fakeDiscordClient = {
+  channels: {
+    cache: new Map([
+      ["123456789012345678", { send: (msg) => console.log("[Fake Discord] " + msg) }]
+    ])
+  }
+};
+
+const salleServeur = new Room("Salle Serveur");
+
+salleServeur.addSensor(new CamAdapter("Entrée"));
+salleServeur.addSensor(new TempSensAdapter("Rack 3", 37));
+salleServeur.addSensor(new MotionSensAdapter("Couloir"));
+salleServeur.addSensor(new ThermalSensBAdapter("Baie 7"));
+
+salleServeur.addObserver(new ConsoleObserver());
+salleServeur.addObserver(new EmailObserver());
+salleServeur.addObserver(new DiscordObserver());
+salleServeur.addObserver(new LogObserv(fakeDiscordClient, "./logs.txt", "123456789012345678"));
+
+salleServeur.checkSensors();
